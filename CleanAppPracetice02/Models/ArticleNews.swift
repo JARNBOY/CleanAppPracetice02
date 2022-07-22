b1b76090e58803d0be47caaf01da1303efcb6d6b
Papon <papon.s@lawyerplusgroup.com>
2022-07-22 10:43:43 +0700

    add endpoint api load News

---
+++ CleanAppPracetice02/Models/ArticleNews.swift
@@ -0,0 +1,25 @@
+//
+//  ArticleNews.swift
+//  CleanAppPracetice02
+//
+//  Created by Papon Supamongkonchai on 22/7/2565 BE.
+//
+
+import Foundation
+
+struct ArticleList:Codable{
+    let articles:[Article]
+}
+
+extension ArticleList{
+    static var all : Resource<ArticleList> = {
+        let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=0bfb7217d29740c3a8ca13fda1c95a49")!
+        return Resource(url: url)
+    }()
+}
+
+struct Article:Codable{
+    let author:String?
+    let title:String
+    let description:String?
+}
